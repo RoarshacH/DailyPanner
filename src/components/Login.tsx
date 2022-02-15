c611d9cb74ce29290779e46c03e97298fa78be6c
RoarshacH <vid.dilshan@gmail.com>
2022-02-15 10:53:05 -0500

    Finished Login Screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,63 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, SafeAreaView, Platform } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 const Login = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
-      <View style={[styles.box1, styles.boxStyle]}>
-        <View style={[styles.box1_1]}></View>
-        <View style={[styles.box1_2]}>
+      <View style={styles.top}>
+        <View style={styles.box1_1}></View>
+        <View style={styles.box1_2}>
           <Text style={styles.headerTextStyle}>Welcome</Text>
         </View>
       </View>
 
-      <View style={[styles.box2, styles.boxStyle]}>
-        <View style={[styles.box2_1]}>{/* <Text>Header Text</Text> */}</View>
-        <View style={[styles.box2_2]}></View>
+      <View style={styles.body}>
+        <TextInput style={styles.input} value={"Username"} />
+        <TextInput
+          style={styles.input}
+          value={"Password"}
+          placeholder="Password"
+        />
+        <Text style={styles.smallText}> Forgot Password</Text>
+      </View>
+      <View style={styles.buttonSet}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}> LogIn </Text>
+        </TouchableOpacity>
+
+        <View style={styles.signUpRow}>
+          <TouchableOpacity style={styles.signUpImages}>
+            <Image
+              style={styles.signUpImages}
+              source={require("./../../assets/images/facebook.png")}
+            ></Image>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.signUpImages}>
+            <Image
+              style={styles.signUpImages}
+              source={require("./../../assets/images/Instagram.png")}
+            ></Image>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.signUpImages}>
+            <Image
+              style={styles.signUpImages}
+              source={require("./../../assets/images/twitter.png")}
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.smallText}> Not a User Sign up Here!</Text>
       </View>
     </SafeAreaView>
   );
@@ -24,17 +68,27 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
-  boxStyle: {},
-  box1: {
-    marginTop: 40,
-    // backgroundColor: "green",
+  top: {
+    marginTop: "10%",
     flex: 2,
     justifyContent: "flex-end",
   },
-  box2: {
-    // backgroundColor: "orange",
-    flex: 3,
-    justifyContent: "flex-end",
+  body: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: "15%",
+  },
+  footer: {
+    flex: 0.5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  buttonSet: {
+    flex: 1.5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: "10%",
   },
   box1_1: {
     backgroundColor: "blue",
@@ -52,18 +106,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  box2_1: {
-    backgroundColor: "teal",
-    marginTop: 5,
-    height: 80,
-  },
-  box2_2: {
-    backgroundColor: "teal",
-    marginTop: 5,
-    height: 40,
-    position: "relative",
-    marginBottom: 40,
-  },
   headerTextStyle: {
     fontSize: 24,
     ...Platform.select({
@@ -77,12 +119,46 @@ const styles = StyleSheet.create({
       },
     }),
   },
-  defaultContainer: {
-    flex: 1,
-    fontFamily: "sans-serif",
+  input: {
+    height: 50,
+    margin: 12,
+    borderColor: "gray",
+    borderRadius: 10,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  button: {
     backgroundColor: "gray",
-    alignItems: "center",
+    height: 50,
+    width: 150,
+    borderRadius: 10,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  smallText: {
+    position: "relative",
+    bottom: -10,
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  signUpRow: {
+    flex: 0.2,
+    flexDirection: "row",
+    position: "relative",
+    paddingTop: 50,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  signUpImages: {
+    margin: 10,
+    borderRadius: 50,
+    width: 50,
+    height: 50,
   },
 });
 
