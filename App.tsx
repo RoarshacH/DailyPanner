0176e9445a2fb36f25160845010aa19bc9c20a92
RoarshacH <vid.dilshan@gmail.com>
2022-02-23 18:20:57 -0500

    remove some merge conflicts

--- App.tsx
+++ App.tsx
@@ -4,11 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./src/screens/Login";
 import SignUpScreen from "./src/screens/SignUP";
 import HomeScreen from "./src/screens/Home";
-<<<<<<< HEAD
 import ScrollViewScreen from "./src/screens/Scrolling";
-=======
 import DashboardView from "./src/screens/Dashboard";
->>>>>>> 2b2e1f476f73592c7b72c4b1a09aaf637ac565d4
 
 export default function App() {
   return (
@@ -16,13 +13,10 @@ export default function App() {
       {/* //Uncomment the Screen that needs viewing and save the file  default is Login Screen*/}
       {/* <LoginScreen></LoginScreen> */}
       {/* <SignUpScreen></SignUpScreen> */}
-<<<<<<< HEAD
       <HomeScreen></HomeScreen>
       {/* <ScrollViewScreen></ScrollViewScreen> */}
-=======
       {/* <HomeScreen></HomeScreen> */}
       {/* <DashboardView></DashboardView> */}
->>>>>>> 2b2e1f476f73592c7b72c4b1a09aaf637ac565d4
     </View>
   );
 }
