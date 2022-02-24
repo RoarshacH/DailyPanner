9257b679e9494b32912d94eb0358aa02f8838f30
RoarshacH <vid.dilshan@gmail.com>
2022-02-23 23:43:57 -0500

    New Layout

--- App.tsx
+++ App.tsx
@@ -6,17 +6,20 @@ import SignUpScreen from "./src/screens/SignUP";
 import HomeScreen from "./src/screens/Home";
 import ScrollViewScreen from "./src/screens/Scrolling";
 import DashboardView from "./src/screens/Dashboard";
+import SpaceGameLayout from "./src/screens/SpaceGameLayout";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* //Uncomment the Screen that needs viewing and save the file  default is Login Screen*/}
-      {/* <LoginScreen></LoginScreen> */}
+      <LoginScreen></LoginScreen>
       {/* <SignUpScreen></SignUpScreen> */}
-      <HomeScreen></HomeScreen>
+      {/* <HomeScreen></HomeScreen> */}
       {/* <ScrollViewScreen></ScrollViewScreen> */}
       {/* <HomeScreen></HomeScreen> */}
       {/* <DashboardView></DashboardView> */}
+
+      {/* <SpaceGameLayout></SpaceGameLayout> */}
     </View>
   );
 }
